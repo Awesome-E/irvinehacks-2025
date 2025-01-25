540bc7c1c9d76e8484005c9205ad1c33e9fd3d99
Jacksopop <75774890+Jacksopop@users.noreply.github.com>
2025-01-25 11:31:12 -0800

    fixed typo in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 const Header: FC = () => {
   return <div className="header">
-    <h1>Title Thign</h1>
+    <h1>Title Thing</h1>
 
     <button>NEW</button> (this actually opens the receipt or something)
   </div>
