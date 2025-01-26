cd989a4d9ff4fd224f69f5b1d4614fce47f03856
Ashley Zhou <azhou901@gmail.com>
2025-01-25 18:58:15 -0800

    receipt list fetching & loading

--- src/components/ReceiptList.tsx
+++ src/components/ReceiptList.tsx
@@ -2,7 +2,7 @@ import './ReceiptList.scss';
 import UploadForm from './UploadForm'
 import { faReceipt, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { DateTime } from "luxon";
 
 interface ReceiptProps {
@@ -28,8 +28,53 @@ const Receipt: FC<ReceiptProps> = ({ name, date }) => {
 
 const ReceiptList: FC = () => {
   const [showUploadModal, setShowUploadModal] = useState(false)
+  const [receipts, setReceipts] = useState<ReceiptProps[]>();
+
+  const fetchReceipts = async (): Promise<ReceiptProps[]> => {
+    try {
+      const response = await fetch('/api/receipts');
+      if (response.ok) {
+        const data = await response.json();
+        return data.map((receipt: ReceiptProps) => ({
+          ...receipt,
+          date: new Date(receipt.date),
+        }));
+      } else {
+        console.log('Fetch failed :(');
+        return [];
+      }
+    } catch (error) {
+      console.error('Error fetching receipts:', error);
+      return [];
+    }
+  };
+
+
+  useEffect(() => {
+    const loadReceipts = async () => {
+      const data = await fetchReceipts();
+      if (!data.length) {
+        const tempReceipts: ReceiptProps[] = [
+          { name: "Walmart", date: new Date("2025-01-01") },
+          { name: "Target", date: new Date("2025-01-15") },
+          { name: "Costco", date: new Date("2025-01-20") },
+        ];
+        setReceipts(tempReceipts);
+      } else {
+        setReceipts(data);
+      }
+    };
+
+    loadReceipts();
+  }, []);
+
   return <div className="receipt-list">
-    {[1,2,3,4,5].map(r => <Receipt name={'Receipt ' + r} date={new Date()} key={r}/>)}
+    {/* {[1,2,3,4,5].map(r => <Receipt name={'Receipt ' + r} date={new Date()} key={r}/>)} */}
+    {receipts && receipts.map((r, index) => (
+  <Receipt name={`Receipt ${r.name}`} date={r.date} key={index} />
+))}
+
+    {/* handle receipt displays */}
     <div className="receipt new-receipt">
         <UploadForm open={showUploadModal} setOpen={setShowUploadModal} />
         <button className="new-box" onClick={() => setShowUploadModal(!showUploadModal)}> + New</button>
